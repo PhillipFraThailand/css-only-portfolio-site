9bce4d537ff49d18b610adc6a9af6aea3dda2b97
phillipeismark <phillip_eismark@hotmail.dk>
2024-01-10 17:11:26 +0100

    add lenis smooth scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 import { HeroSection } from "../app/components/hero-section/HeroSection";
+import Lenis from "@studio-freight/lenis";
 
 export default function Home() {
+  // Only run when component mounts (not SSR)
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", (e: any) => {});
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <>
       <section>
         <h1 className="name">Phillip Eismark</h1>
       </section>
+      <div className="box" style={{ backgroundColor: "red" }} />
+      <div className="box" style={{ backgroundColor: "blue" }} />
+      <div className="box" style={{ backgroundColor: "green" }} />
 
       {/* <HeroSection /> */}
       {/* <section style={{ backgroundColor: "red" }}>About</section>
