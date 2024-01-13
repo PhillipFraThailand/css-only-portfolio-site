8ccdb1ed6710ac6276068cf0dc3a5f9320ed64e9
phillipeismark <phillip_eismark@hotmail.dk>
2024-01-13 12:00:23 +0100

    lenis scroll, scroll indicator animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Lenis from "@studio-freight/lenis";
-
+import { AuroraBackground } from "@/app/components/aurora-background/AuroraBackground";
 export default function Home() {
-  // Only run when component mounts (not SSR)
+  console.log(AuroraBackground({ children: "Hello" }));
+  const [isScrollIndicatorVisible, setIsScrollIndicatorVisible] =
+    useState(true);
+
+  // Only after mounting the DOM, since lenis needs to subscribe to the scroll event
   useEffect(() => {
     const lenis = new Lenis();
-
-    lenis.on("scroll", (e: any) => {});
-
-    function raf(time: any) {
+    const raf = (time: unknown) => {
       lenis.raf(time);
       requestAnimationFrame(raf);
-    }
+    };
 
     requestAnimationFrame(raf);
+
+    lenis.on("scroll", (e: unknown) => {
+      const scrollPosition = window.scrollY; // Scroll progress
+      const windowHeight = window.innerHeight; // Viewport height
+      const documentHeight = document.documentElement.scrollHeight; // Total document height
+
+      // When scrolled past set amount set state to hide scroll indicator
+      if (scrollPosition > windowHeight * 0.1) {
+        setIsScrollIndicatorVisible(false);
+      } else {
+        setIsScrollIndicatorVisible(true);
+      }
+    });
   }, []);
   return (
     <>
       <section>
-        <h1 className="name">Phillip Eismark</h1>
-        <h1 className="name">Full Stack App Developer</h1>
+        <h1 className="header">Phillip Eismark</h1>
+        <h1 className="header">Mobile App Developer</h1>
+        <h1 className="header">Full Stack</h1>
+        <h1 className="header">Websites</h1>
+        {isScrollIndicatorVisible && <div className="scrollIndicator" />}
+        <p className="header">Scroll</p>
       </section>
       <div className="box" style={{ backgroundColor: "red" }} />
       <div className="box" style={{ backgroundColor: "blue" }} />
